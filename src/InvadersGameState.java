90bf3b4355a502e0c2a352e1ef481d86ea4ecfd6
Devin Joubert <dvnjoubert0@gmail.com>
2018-04-01 20:45:48 +0200

    Final upload

--- src/InvadersGameState.java
+++ src/InvadersGameState.java
@@ -1,7 +1,7 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Random;
 
 public class InvadersGameState {
 	private final double fps = 240.0;
@@ -9,20 +9,20 @@ public class InvadersGameState {
 
 	private final int default_canvas_size = 512;
 	private final double default_scale = 100;
-	
+
 	private final double default_player_acc = 700;
 	private final double default_player_friction = default_player_acc;
 	private final double default_player_turn_rate = 2;
 	private final double default_player_bounce_coeff = 0.5;
-	
+
 	private final double default_shoot_delay = 0.2; //second
 	private final int default_shoot_count_max = (int) (fps * default_shoot_delay);
-	
+
 	private final double default_enemy_radius = 6;
-	private final double default_enemy_velocity = 2;
+	private final double default_enemy_velocity = 10;
 	private final Polygon default_enemy_model = Polygon.circle(default_enemy_radius, 10);
-	private final int default_enemy_rows = 1; //5
-	private final int default_enemy_coloums = 1; //9
+	private final int default_enemy_rows = 4; //5
+	private final int default_enemy_coloums = 10; //9
 	private final double default_enemy_gap = 6;
 	private final double default_enemy_start_height = default_scale - 20;
 
@@ -30,13 +30,15 @@ public class InvadersGameState {
 	private Player player;
 	private ArrayList<GameObject> projectiles;
 	private ArrayList<GameObject> enemies;
-	
+
 	private int shootDelay = 0;
 	private int playerScore = 0;
 
 	private boolean shoot = true;
 	private boolean shotFired = false;
-	
+	private boolean over = false;
+	private boolean win = false;
+
 	public InvadersGameState() {
 		this.player = new Player();
 		this.canvas = new Draw();
@@ -70,17 +72,17 @@ public class InvadersGameState {
 				this.shotFired = true;
 			} else this.shoot = true;
 		}
-		
+
 		Iterator<GameObject> pr = this.projectiles.iterator();
-		
+
 		this.player.move(playerAcc, dt / 1000.0);
 		this.player.rotate(playerTurn * dt / 1000.0);
 		for (GameObject p : this.projectiles) p.move(new Vector(0, 0), dt / 1000.0);
 		for (GameObject e : this.enemies) e.move(new Vector(0, 0), dt / 1000.0);
-		
+
 		pr = this.projectiles.iterator();
 		while (pr.hasNext()) if (pr.next().getPosition().magnitude() > 150) pr.remove();
-		
+
 		if (this.player.outside(default_scale)) this.player.bounce(default_player_bounce_coeff);
 		if (this.player.getAngularPosition() < 0) this.player.setAngle(0);
 		if (this.player.getAngularPosition() > Math.PI) this.player.setAngle(Math.PI);
@@ -91,8 +93,12 @@ public class InvadersGameState {
 			this.shotFired = false;
 			this.player.reload();
 		}
-		
+
 		checkIntersections();
+		if (this.enemies.size() == 0) {
+			this.over = true;
+			this.win = true;
+		}
 	}
 
 	public void buffer() {
@@ -107,46 +113,48 @@ public class InvadersGameState {
 		for (GameObject e : this.enemies) e.draw(this.canvas);
 		printScore();
 	}
-	
+
 	private void checkIntersections() {
 		ArrayList<GameObject> removede = new ArrayList<>();
 		ArrayList<GameObject> removedp = new ArrayList<>();
-		
+
 		for (Iterator<GameObject> enemies = this.enemies.iterator(); enemies.hasNext();) {
 			GameObject enemy = enemies.next();
 			for (Iterator<GameObject> projectiles = this.projectiles.iterator(); projectiles.hasNext();) {
 				GameObject projectile = projectiles.next();
-				
+
 				if (enemy.getPosition().minus(projectile.getPosition()).magnitude() < 6) {
 					playerScore++;
 					removede.add(enemy);
 					removedp.add(projectile);
 				}
 			}
+
+			if (enemy.getPosition().minus(this.player.getPosition()).magnitude() < 10 || enemy.getPosition().getComponent(1) < -default_scale) this.over = true;
 		}
-		
+
 		this.projectiles.removeAll(removedp);
 		this.enemies.removeAll(removede);
 	}
-	
+
 	private void printScore() {
 		this.canvas.setFont();
 		this.canvas.text(-(default_scale - 15), default_scale - 6, "Score: " + this.playerScore);
 	}
-	
+
 	public boolean isOver() {
-		if (this.enemies.size() == 0) return true;
+		if (this.over) return true;
 		else return false;
 	}
-	
+
 	private void createEnemies() {
 		double startingPosition = 0;
 		double diameter = 2 * default_enemy_radius;
-		if (default_enemy_coloums % 2 == 0) startingPosition = -(((default_enemy_coloums / 2) - 0.5) * (default_enemy_gap + diameter));
+		if (default_enemy_coloums % 2 == 0) startingPosition = -(((default_enemy_coloums / 2) - 0.5) * (default_enemy_gap + diameter)); //This is just to find out the starting position to arrange the enemies in a grid
 		else startingPosition = -(((default_enemy_coloums - 1) / 2) * (default_enemy_gap + diameter));
-		
+
 		double delta = diameter + default_enemy_gap;
-		
+
 		for (int i = 0; i < default_enemy_rows; i++) {
 			for (int j = 0; j < default_enemy_coloums; j++) {
 				Vector position = new Vector(startingPosition + (j * delta), default_enemy_start_height - (i * delta));
@@ -155,29 +163,72 @@ public class InvadersGameState {
 			}
 		}
 	}
-	
+
+	public void endScreen() {
+		String text;
+
+		if (this.win) text = String.format("YOU WIN, R TO RESTART OR Q TO QUIT");
+		else text = "YOU LOSE, R TO RESTART OR Q TO QUIT";
+
+		this.canvas.setPenColor(Color.WHITE);
+		this.canvas.filledRectangle(0, 0, default_scale, default_scale);
+		this.canvas.setPenColor(Color.BLACK);
+		this.canvas.text(0, 10, text);
+		this.canvas.text(0, 0, String.format("YOUR SCORE: %d", this.playerScore));
+		
+		this.buffer();
+	}
+
 	public void blackScreen() {
 		this.canvas.clear();
 		this.buffer();
 	}
-	
+
+	public void displayIntro() {
+		this.canvas.setPenColor(Color.WHITE);
+		this.canvas.filledRectangle(0, 0, default_scale / 2, default_scale / 2);
+		this.canvas.setPenColor(Color.BLACK);
+		this.canvas.text(0, 50, "SPACE INVADERS");
+		this.canvas.text(0, 20, "USE A AND D TO ROTATE THE TURRET");
+		this.canvas.text(0, 10, "USE LEFT AND RIGHT TO MOVE THE PLAYER");
+		this.canvas.text(0, 0, "USE SPACE TO SHOOT");
+		this.canvas.text(0, -10, "PRESS Q TO QUIT OR ANY OTHER KEY TO CONTINUE");
+		
+		this.buffer();
+	}
+
 	public boolean quitPressed() {
 		if (this.canvas.isKeyPressed(KeyEvent.VK_Q)) return true;
 		else return false;
 	}
 
-	public static void main(String[] args) {
-		InvadersGameState gs = new InvadersGameState();
-
-		while (true) {
-			gs.updateAll();
-			
-			if (!gs.isOver()) {
-				gs.drawAll();
-				gs.buffer();
-			} else break;
-		}
+	public boolean keyPressed() {
+		if (this.canvas.hasNextKeyTyped()) return true;
+		else return false;
+	}
+	
+	public boolean mustRestart() {
+		if (this.canvas.isKeyPressed(KeyEvent.VK_R)) return true;
+		else return false;
+	}
+	
+	public void dispose() {
+		this.canvas.dispose();
+	}
+	
+	public void restart() {
+		this.player = new Player();
+		this.projectiles = new ArrayList<GameObject>();
+		this.enemies = new ArrayList<GameObject>();
+		createEnemies();
+		initCanvas();
 		
-		while (!gs.quitPressed());
+		shootDelay = 0;
+		playerScore = 0;
+
+		this.shoot = true;
+		this.shotFired = false;
+		this.over = false;
+		this.win = false;
 	}
 }
